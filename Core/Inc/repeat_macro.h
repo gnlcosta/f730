d86cd8a44bc66c9617dea6f21ed7b8403c3802e5
Gianluca Costa <g.costa@xplico.org>
2020-09-01 19:13:46 +0200

    Test the dual-spi flash bus, using the FW in the ex-flash and the i-cache

---
+++ Core/Inc/repeat_macro.h
@@ -0,0 +1,28 @@
+
+
+#ifndef __REPEAT_MACRO_H__
+#define __REPEAT_MACRO_H__
+
+// see https://stackoverflow.com/questions/8551418/c-preprocessor-macro-for-returning-a-string-repeated-a-certain-number-of-times
+
+#define REP0(X)
+#define REP1(X) X
+#define REP2(X) REP1(X) X
+#define REP3(X) REP2(X) X
+#define REP4(X) REP3(X) X
+#define REP5(X) REP4(X) X
+#define REP6(X) REP5(X) X
+#define REP7(X) REP6(X) X
+#define REP8(X) REP7(X) X
+#define REP9(X) REP8(X) X
+#define REP10(X) REP9(X) X
+
+#define REP(THOUSANDS,HUNDREDS,TENS,ONES,X) \
+  REP##THOUSANDS(REP10(REP10(REP10(X)))) \
+  REP##HUNDREDS(REP10(REP10(X))) \
+  REP##TENS(REP10(X)) \
+  REP##ONES(X)
+
+
+#endif
+
--- Core/Src/business_logic.c
+++ Core/Src/business_logic.c
@@ -1,16 +1,32 @@
 #include "main.h"
 #include "cmsis_os.h"
 
+#include "repeat_macro.h"
+
 #include "app_config.h"
+
 
 #if APP_LED_TOGGLE_EN
 
+
 void BusinessLogic(void)
 {
+	volatile double a = 1;
+	volatile uint32_t i = 0;
+
 	/* Infinite loop */
 	for(;;)  {
-	  HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
-	  osDelay(500);
+		// this code tests the performance (good pcb routing) of the Dual/Quad-SPI flash. If the led stop to blink the SPI clock must be reduced (it is likely that depend by a bad PCB routing)
+		REP(0, 5, 0, 0, i++;a*=i;);
+		REP(0, 5, 0, 0, i++;a*=2*i;);
+		REP(0, 5, 0, 0, i++;a*=3*i;);
+		REP(0, 5, 0, 0, i++;a*=4*i;);
+		REP(0, 5, 0, 0, i++;a*=5*i;);
+		REP(0, 5, 0, 0, i++;a*=6*i;);
+		REP(1, 5, 0, 0, i++;a*=7*i;);
+
+		HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+		osDelay(25);
 	}
 }
 
