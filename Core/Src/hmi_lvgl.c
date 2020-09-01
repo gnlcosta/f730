3b5a9bb8b28c996b4f7a9ec7a000c1c7b9167a6a
Gianluca Costa <g.costa@xplico.org>
2020-09-01 19:57:07 +0200

    LVGL simple example

--- Core/Src/hmi_lvgl.c
+++ Core/Src/hmi_lvgl.c
@@ -6,30 +6,101 @@
 
 #if APP_LVGL_BUTTON_EN
 
-static void btn_event_cb(lv_obj_t * btn, lv_event_t event)
-{
-    if(event == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
 
-        /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t * label = lv_obj_get_child(btn, NULL);
-        lv_label_set_text_fmt(label, "Button: %d", cnt);
-    }
-}
 
 /**
- * Create a button with a label and react on Click event.
+ * Create styles from scratch for buttons.
  */
 void lv_ex_get_started(void)
 {
+    static lv_style_t style_btn;
+    static lv_style_t style_btn_red;
+
+    /*Create a simple button style*/
+    lv_style_init(&style_btn);
+    lv_style_set_radius(&style_btn, LV_STATE_DEFAULT, 10);
+    lv_style_set_bg_opa(&style_btn, LV_STATE_DEFAULT, LV_OPA_COVER);
+    lv_style_set_bg_color(&style_btn, LV_STATE_DEFAULT, LV_COLOR_SILVER);
+    lv_style_set_bg_grad_color(&style_btn, LV_STATE_DEFAULT, LV_COLOR_GRAY);
+    lv_style_set_bg_grad_dir(&style_btn, LV_STATE_DEFAULT, LV_GRAD_DIR_VER);
+
+    /*Swap the colors in pressed state*/
+    lv_style_set_bg_color(&style_btn, LV_STATE_PRESSED, LV_COLOR_GRAY);
+    lv_style_set_bg_grad_color(&style_btn, LV_STATE_PRESSED, LV_COLOR_SILVER);
+
+    /*Add a border*/
+    lv_style_set_border_color(&style_btn, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_style_set_border_opa(&style_btn, LV_STATE_DEFAULT, LV_OPA_70);
+    lv_style_set_border_width(&style_btn, LV_STATE_DEFAULT, 2);
+
+    /*Different border color in focused state*/
+    lv_style_set_border_color(&style_btn, LV_STATE_FOCUSED, LV_COLOR_BLUE);
+    lv_style_set_border_color(&style_btn, LV_STATE_FOCUSED | LV_STATE_PRESSED, LV_COLOR_NAVY);
+
+    /*Set the text style*/
+    lv_style_set_text_color(&style_btn, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+
+    /*Make the button smaller when pressed*/
+    lv_style_set_transform_height(&style_btn, LV_STATE_PRESSED, -5);
+    lv_style_set_transform_width(&style_btn, LV_STATE_PRESSED, -10);
+
+    /*Add a transition to the size change*/
+    static lv_anim_path_t path;
+    lv_anim_path_init(&path);
+    lv_anim_path_set_cb(&path, lv_anim_path_overshoot);
+
+    lv_style_set_transition_prop_1(&style_btn, LV_STATE_DEFAULT, LV_STYLE_TRANSFORM_HEIGHT);
+    lv_style_set_transition_prop_2(&style_btn, LV_STATE_DEFAULT, LV_STYLE_TRANSFORM_WIDTH);
+    lv_style_set_transition_time(&style_btn, LV_STATE_DEFAULT, 300);
+    lv_style_set_transition_path(&style_btn, LV_STATE_DEFAULT, &path);
+
+
+    /*Create a red style. Change only some colors.*/
+    lv_style_init(&style_btn_red);
+    lv_style_set_bg_color(&style_btn_red, LV_STATE_DEFAULT, LV_COLOR_RED);
+    lv_style_set_bg_grad_color(&style_btn_red, LV_STATE_DEFAULT, LV_COLOR_MAROON);
+    lv_style_set_bg_color(&style_btn_red, LV_STATE_PRESSED, LV_COLOR_MAROON);
+    lv_style_set_bg_grad_color(&style_btn_red, LV_STATE_PRESSED, LV_COLOR_RED);
+    lv_style_set_text_color(&style_btn_red, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+
+    /*Create buttons and use the new styles*/
     lv_obj_t * btn = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
-    lv_obj_set_event_cb(btn, btn_event_cb);                 /*Assign a callback to the button*/
+    lv_obj_set_pos(btn, 5, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 110, 50);                          /*Set its size*/
+    lv_obj_reset_style_list(btn, LV_BTN_PART_MAIN);         /*Remove the styles coming from the theme*/
+    lv_obj_add_style(btn, LV_BTN_PART_MAIN, &style_btn);
 
     lv_obj_t * label = lv_label_create(btn, NULL);          /*Add a label to the button*/
     lv_label_set_text(label, "Button");                     /*Set the labels text*/
+
+    /*Create a new button*/
+    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), btn);
+    lv_obj_set_pos(btn2, 125, 10);
+    lv_obj_set_size(btn2, 110, 50);                             /*Set its size*/
+    lv_obj_reset_style_list(btn2, LV_BTN_PART_MAIN);         /*Remove the styles coming from the theme*/
+    lv_obj_add_style(btn2, LV_BTN_PART_MAIN, &style_btn);
+    lv_obj_add_style(btn2, LV_BTN_PART_MAIN, &style_btn_red);   /*Add the red style on top of the current */
+    lv_obj_set_style_local_radius(btn2, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_RADIUS_CIRCLE); /*Add a local style*/
+
+    label = lv_label_create(btn2, NULL);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button 2");                     /*Set the labels text*/
+
+    /*Describe the color for the needles*/
+    static lv_color_t needle_colors[3];
+    needle_colors[0] = LV_COLOR_BLUE;
+    needle_colors[1] = LV_COLOR_ORANGE;
+    needle_colors[2] = LV_COLOR_PURPLE;
+
+    /*Create a gauge*/
+    lv_obj_t * gauge1 = lv_gauge_create(lv_scr_act(), NULL);
+    lv_gauge_set_needle_count(gauge1, 3, needle_colors);
+    lv_obj_set_size(gauge1, 200, 200);
+    lv_obj_align(gauge1, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -20);
+
+    /*Set the values*/
+    lv_gauge_set_value(gauge1, 0, 10);
+    lv_gauge_set_value(gauge1, 1, 20);
+    lv_gauge_set_value(gauge1, 2, 30);
 }
 
 
